3997eaad94569b93c482683676ac638851d2e338
Zhou, Yang (133-Extern) <futuremove.zhou@daimler.com>
2018-09-28 17:27:48 +0800

    Initial commit

---
+++ app/src/main/java/module/base/baseframwork/base/BaseFragmentView.java
@@ -0,0 +1,8 @@
+package module.base.baseframwork.base;
+
+import android.os.Bundle;
+
+public interface BaseFragmentView extends BaseView{
+   public Bundle getBundle();
+    public BaseFragment getFragment();
+}
---
+++ app/src/main/java/module/base/baseframwork/base/BaseView.java
@@ -0,0 +1,9 @@
+package module.base.baseframwork.base;
+
+public interface BaseView {
+    /**
+     * 切换夜间模式
+     * @param isNight 是否切换为夜间模式
+     */
+    void isNightMode(boolean isNight);
+}
