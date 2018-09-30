49aa6b4489c834e3800dc05f17213af468f4297d
zhouyang <814575934@qq.com>
2018-09-30 21:06:22 +0800

    开始封装recycleview

--- app/src/main/java/module/base/baseframwork/base/TestActivity.java
+++ app/src/main/java/module/base/baseframwork/base/TestActivity.java
@@ -1,9 +1,9 @@
 package module.base.baseframwork.base;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 import module.base.baseframwork.R;
-
-import android.os.Bundle;
 
 public class TestActivity extends AppCompatActivity {
 
@@ -11,5 +11,6 @@ public class TestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+
     }
 }
