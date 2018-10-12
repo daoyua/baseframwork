cb635b8a8f1a018ded03dafefd234c1464759a72
Zhou, Yang (133-Extern) <futuremove.zhou@daimler.com>
2018-10-12 17:33:23 +0800

    集成一个抓取异常到baseapplication和一个loguntils工具类

--- app/src/main/java/module/base/baseframwork/base/BaseApplication.java
+++ app/src/main/java/module/base/baseframwork/base/BaseApplication.java
@@ -1,15 +1,25 @@
 package module.base.baseframwork.base;
 
 import android.app.Application;
+import android.content.Context;
 
+import module.base.baseframwork.CrashHandler;
 import module.base.baseframwork.base.retrofit.RetrofitFactory;
 
 
 public abstract class BaseApplication extends Application {
+    public static Context mContext;
     @Override
     public void onCreate() {
         super.onCreate();
+        /**
+         * 初始化抓去异常
+         * */
+        CrashHandler crashHandler = CrashHandler.getInstance();
+        crashHandler.init(getApplicationContext());
+        mContext = this;
         RetrofitFactory.setHostUrl(initHostUrl());
+
     }
     public abstract String initHostUrl();
 }
