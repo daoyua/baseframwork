4a4ec425811dde4b4d54a6f169417ef6cb33cbfb
zhouyang <814575934@qq.com>
2018-10-12 22:17:04 +0800

    添加myCompositeDisposable

--- app/src/main/java/module/base/baseframwork/base/presenter/BasePresenter.java
+++ app/src/main/java/module/base/baseframwork/base/presenter/BasePresenter.java
@@ -2,17 +2,19 @@ package module.base.baseframwork.base.presenter;
 
 import android.os.Bundle;
 
+import io.reactivex.disposables.CompositeDisposable;
 import module.base.baseframwork.base.view.BaseView;
 
 public abstract class BasePresenter <T extends BaseView>{
 
     protected T mView;
-//    protected CompositeSubscription compositeSubscription;
+    protected CompositeDisposable myCompositeDisposable;
     /**
      * 绑定View
      */
     public void onAttch(T view) {
         this.mView = view;
+        myCompositeDisposable=new CompositeDisposable();
     }
     /**
      * 做初始化的操作,需要在view的视图初始化完成之后才能调用
@@ -24,6 +26,9 @@ public abstract class BasePresenter <T extends BaseView>{
      */
     public void onDestroy() {
         mView = null;
+
+        myCompositeDisposable.dispose();
+//        compositeDisposable.clear();
     }
     /**
      * 容易被回收掉时保存数据
