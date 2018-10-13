bb22c5c073c43d66a0bdd41707837712ddcd6d53
zhouyang <814575934@qq.com>
2018-10-13 16:26:58 +0800

    TabFragmentPagerAdapter

---
+++ app/src/main/java/module/base/baseframwork/base/adapter/TabFragmentPagerAdapter.java
@@ -0,0 +1,25 @@
+package module.base.baseframwork.base.adapter;
+
+import java.util.ArrayList;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class TabFragmentPagerAdapter extends FragmentPagerAdapter {
+    ArrayList<Fragment> listFragments;
+    public TabFragmentPagerAdapter(FragmentManager fm, ArrayList<Fragment> list) {
+        super(fm);
+        listFragments=list;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return listFragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return listFragments.size();
+    }
+}
