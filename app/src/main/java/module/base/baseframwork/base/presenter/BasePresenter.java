1e17f67a6f564f676ab7fc97a726ba88f9bced48
zhouyang <814575934@qq.com>
2018-10-20 12:34:15 +0800

    rxbus

--- app/src/main/java/module/base/baseframwork/base/presenter/BasePresenter.java
+++ app/src/main/java/module/base/baseframwork/base/presenter/BasePresenter.java
@@ -1,10 +1,15 @@
 package module.base.baseframwork.base.presenter;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+import module.base.baseframwork.base.rxbus.Event;
+import module.base.baseframwork.base.rxbus.RxBus;
 import module.base.baseframwork.base.view.BaseView;
+import module.base.baseframwork.untils.LogUtils;
 
 public abstract class BasePresenter< T extends BaseView>  {
 
@@ -47,4 +52,32 @@ public abstract class BasePresenter< T extends BaseView>  {
      * 容易被回收掉时保存数据
      */
     public abstract void onSaveInstanceState(Bundle outState);
+
+
+    protected void openRxbus(boolean isOpen){
+        if (isOpen){
+            //注册eventbus
+            Disposable disposable = RxBus.getDefault().register(Event.class, new Consumer<Event>() {
+                @Override
+                public void accept(Event event) {
+                    int eventCode = event.getCode();
+                    Log.e("RxBus", event.toString());
+                    switch (eventCode) {
+                        case Event.EVENT_CLOSE_ALL_ACTIVITY:
+                            LogUtils.e("1000:");
+                            break;
+                        default:
+//                            LogUtils.e("no!!!!!!!!wrong");
+                            Log.e("RxBus", event.toString());
+                            onEvent(event);
+                            break;
+                    }
+                }
+            });
+            addDispos(disposable);
+            LogUtils.e("Rxbus is opened");
+        }
+    }
+
+    public abstract void onEvent(Event event);
 }
